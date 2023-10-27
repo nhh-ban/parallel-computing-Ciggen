e6cd38b18e2aef3c4448b819ae1ed31f6bb4c0aa
Sondre Siger <sondre.siger@student.nhh.no>
2023-10-27 18:04:37 +0200

    second commit

--- timer.R
+++ timer.R
@@ -28,6 +28,22 @@ print(results)
 
 # Comment to results --------
 
+# Result solution 1: Running scripts/solution1.R: 35.567 sec elapsed
+# Result solution 2: Running scripts/solution2.R: 18.477 sec elapsed
+# Result solution 3: Running scripts/solution3.R: 18.468 sec elapsed
+
+# Solution 1 is most definitely slower than solution 2 and 3. 
+# Solution 1 uses a sequential process, limited to one CPU only. This makes 
+# the computing power less powerful than using several CPUS with the parallel computing 
+# method. 
+
+# Solution 3 is a little bit faster than solution 2. Perhaps this is because the 
+# parallel computing method starts in an earlier function in solution 3 than 
+# than in 2? This makes the computer take advantage of speeding 
+# up its computing power earlier in the script, making the result a bit better? 
+# Note that the difference in the results between 2-3 is minimal, and its hard to 
+# conclude with anything based on this result. 
+
 
 
 
